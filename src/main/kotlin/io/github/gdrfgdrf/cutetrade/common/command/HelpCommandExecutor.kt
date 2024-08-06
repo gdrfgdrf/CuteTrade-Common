1455519f5279110bb684e4a50f28abd5108ee040
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2024-08-07 06:17:11 +0800

    feat: some command executor methods for console

--- src/main/kotlin/io/github/gdrfgdrf/cutetrade/common/command/HelpCommandExecutor.kt
+++ src/main/kotlin/io/github/gdrfgdrf/cutetrade/common/command/HelpCommandExecutor.kt
@@ -2,16 +2,28 @@ package io.github.gdrfgdrf.cutetrade.common.command
 
 import io.github.gdrfgdrf.cutetrade.common.extension.translationScope
 import io.github.gdrfgdrf.cutetrade.common.proxy.PlayerProxy
+import io.github.gdrfgdrf.cutetrade.common.translation.ConsoleTranslationScopeAgent
 
 object HelpCommandExecutor {
-    fun execute(playerProxy: PlayerProxy) {
-        playerProxy.translationScope {
-            toCommandTranslation("top")
-                .send("")
-            toCommandTranslation("help")
-                .send("")
-            toCommandTranslation("bottom")
-                .send("")
+    fun execute(playerProxy: PlayerProxy?) {
+        if (playerProxy != null) {
+            playerProxy.translationScope {
+                toCommandTranslation("top")
+                    .send("")
+                toCommandTranslation("help")
+                    .send("")
+                toCommandTranslation("bottom")
+                    .send("")
+            }
+        } else {
+            ConsoleTranslationScopeAgent.apply {
+                toCommandTranslation("top")
+                    .send("")
+                toCommandTranslation("help")
+                    .send("")
+                toCommandTranslation("bottom")
+                    .send("")
+            }
         }
     }
 }
--- src/main/kotlin/io/github/gdrfgdrf/cutetrade/common/command/admin/HelpAdminCommandExecutor.kt
+++ src/main/kotlin/io/github/gdrfgdrf/cutetrade/common/command/admin/HelpAdminCommandExecutor.kt
@@ -2,16 +2,28 @@ package io.github.gdrfgdrf.cutetrade.common.command.admin
 
 import io.github.gdrfgdrf.cutetrade.common.extension.translationScope
 import io.github.gdrfgdrf.cutetrade.common.proxy.PlayerProxy
+import io.github.gdrfgdrf.cutetrade.common.translation.ConsoleTranslationScopeAgent
 
 object HelpAdminCommandExecutor {
-    fun execute(playerProxy: PlayerProxy) {
-        playerProxy.translationScope {
-            toCommandTranslation("top")
-                .send("")
-            toCommandTranslation("admin_help")
-                .send("")
-            toCommandTranslation("bottom")
-                .send("")
+    fun execute(playerProxy: PlayerProxy?) {
+        if (playerProxy != null) {
+            playerProxy.translationScope {
+                toCommandTranslation("top")
+                    .send("")
+                toCommandTranslation("admin_help")
+                    .send("")
+                toCommandTranslation("bottom")
+                    .send("")
+            }
+        } else {
+            ConsoleTranslationScopeAgent.apply {
+                toCommandTranslation("top")
+                    .send("")
+                toCommandTranslation("admin_help")
+                    .send("")
+                toCommandTranslation("bottom")
+                    .send("")
+            }
         }
     }
 }
